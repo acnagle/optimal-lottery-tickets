a8dfa36a3ea27b77b2d15a1017184a31a3e9ba70
saurabh <nouse@time.com>
2020-10-25 16:54:52 -0500

    update pruning bash scripts

--- pruning/base.sh
+++ pruning/base.sh
@@ -5,10 +5,10 @@ hidden_size=500
 
 mkdir -p "./results/LeNet5"
 
-for arch in fc2 fc4; do
+for arch in TwoLayerFC FourLayerFC; do
     mkdir -p "./results/${arch}"
     for epochs in 30 50 100 125 150; do
         python3 main.py --gpu $gpu --lr 0.1 --epochs $epochs --arch $arch --hidden-size $hidden_size --save-results > "./results/${arch}/e${epochs}_h${hidden_size}.out"
-        python3 main.py --gpu $gpu --lr 0.01 --epochs $epochs --arch "LeNet5" --save-results > "./results/LeNet5/e${epochs}.out"
+        python3 main.py --gpu $gpu --lr 0.01 --epochs $epochs --arch "LeNet5" --save-results > "./results/LeNet5/e${epochs}_h${hidden_size}.out"
     done
 done
--- pruning/fc.sh
+++ pruning/fc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 redundancy=5
-gpu=1
+gpu=0
 lr=0.1
 hidden_size=500
 
 for epochs in 30 50 100 125 150; do
     for arch in RedTwoLayerFC RedFourLayerFC; do
-        mkdir -p "./results/{$arch}"
+        mkdir -p "./results/${arch}"
         for sparsity in 0 0.01 0.02 0.025 0.05 0.1 0.25 0.5 0.75 0.9 0.95 0.975 0.98 0.99; do
             python3 main.py --gpu $gpu --lr $lr --epochs $epochs --arch $arch --hidden-size $hidden_size --redundancy $redundancy --sparsity $sparsity --freeze-weights --use-relu --save-results > "./results/${arch}/r${redundancy}_s${sparsity}_e${epochs}_h${hidden_size}_relu.out"
             python3 main.py --gpu $gpu --lr $lr --epochs $epochs --arch $arch --hidden-size $hidden_size --redundancy $redundancy --sparsity $sparsity --freeze-weights --save-results > "./results/${arch}/r${redundancy}_s${sparsity}_e${epochs}_h${hidden_size}.out"
@@ -17,7 +17,7 @@ done
 
 for epochs in 30 50 100 125 150; do
     for arch in WideTwoLayerFC WideFourLayerFC; do
-        mkdir -p "./results/{$arch}"
+        mkdir -p "./results/${arch}"
         for sparsity in 0 0.01 0.02 0.025 0.05 0.1 0.25 0.5 0.75 0.9 0.95 0.975 0.98 0.99; do
             python3 main.py --gpu $gpu --lr $lr --epochs $epochs --arch $arch --hidden-size $hidden_size --redundancy $redundancy --sparsity $sparsity --freeze-weights --save-results > "./results/${arch}/r${redundancy}_s${sparsity}_e${epochs}_h${hidden_size}.out"
         done
